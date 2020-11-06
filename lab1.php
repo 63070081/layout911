484ee494525666bc9f04ad131798fbe8c5f855bd
63070081 <73686016+63070081@users.noreply.github.com>
2020-11-06 15:13:06 +0700

    Add files via upload

---
+++ lab1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lab 1</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 col-lg-30">
+                <img src="wwwmax/img/creative/creative.jpg" style="height: 380px; width: 500px;" class="img-fluid">
+            </div>
+            <div class="col-sm-3">
+                <img src="wwwmax/img/content/office2.jpg" class="img-fluid rounded" />
+                <img src="wwwmax/img/content/office8.jpg" class="img-fluid rounded" />
+                <img src="wwwmax/img/content/office17.jpg" class="img-fluid rounded" />
+            </div> <!-- Close col 1 -->
+            <div class="col-sm-3">
+                <img src="wwwmax/img/staffs/staff7.jpg" class="img-fluid rounded" />
+                <img src="wwwmax/img/banner/ba" class="img-fluid rounded" />
+            </div> <!-- Close col 2 -->
+        </div> <!-- Close row 1 -->
+    </div> <!-- Close container -->
+</body>
+</html>
