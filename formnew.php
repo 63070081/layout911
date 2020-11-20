1d0c5ec450586168d6b87e5cda2ae4f2fe09eb8d
MaxThanit <73686016+63070081@users.noreply.github.com>
2020-11-20 13:03:32 +0700

    Update formnew.php

--- formnew.php
+++ formnew.php
@@ -4,25 +4,14 @@
 	<title>Comment Form</title>
 </head>
 <body>
-<?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'lovelanguage.mysql.database.azure.com', 'maxthanit@lovelanguage', '20082544Max', 'itflab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-$id = $_GET['ID'];
-$res = mysqli_query($conn, 'SELECT * FROM guestbook WHERE id = $id ');
-while($Result = mysqli_fetch_array($res))
-?>
   <form action = "update.php" method = "post" id="CommentForm" >
     Name:<br>
-    <input type="text" name = "name" id="idName" value="<?php echo $Result['Name'];?>"> <br>
+    <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
     Comment:<br>
-    <textarea rows="10" cols="20" name = "comment" id="idComment" value="<?php echo $Result['Comment'];?>"></textarea><br>  
+    <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter Comment"></textarea><br>  
     Link:<br>
-    <input type="text" name = "link" id="idLink" value="<?php echo $Result['Link'];?>"> <br><br>
-    <input type="submit" id="commentBtn" value="<?php echo $Result['ID'];?>">
+    <input type="text" name = "link" id="idLink" placeholder="Enter Link"> <br><br>
+    <input type="submit" id="commentBtn">
   </form> 
 </body>
 </html>
